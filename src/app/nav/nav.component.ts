a3894ee16aec5948e3bafa30d641fe0c2923bd21
Levi Appenfelder <leviapp@Levis-MacBook-Pro.local>
2022-07-01 00:51:57 -0600

    added arrow functionality to change pages

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -11,6 +11,7 @@ export class NavComponent implements OnInit {
   @Input() pageGroup: any = {};
 
   @Output() groupNumber = new EventEmitter()
+  @Output() arrowTime = new EventEmitter()
 
   constructor() { }
 
@@ -19,13 +20,29 @@ export class NavComponent implements OnInit {
   }
 
   ngOnChanges(changes: SimpleChanges) {
-    
-}
 
+}
 
   perPage(e:any) {
     // console.log(e.target.value, "YEH!")
     this.groupNumber.emit(e.target.value)
+  }
+
+  arrowClick(direction: string, current: number) {
+    console.log(direction, current, this.pageInfo["total_pages"])
+    if (direction === "right" && current !== this.pageInfo["total_pages"]) {
+      this.arrowTime.emit(current+1)
+    }
+    else if (direction === "right" && current === this.pageInfo["total_pages"]) {
+      this.arrowTime.emit(1)
+    }
+
+    else if (direction === "left" && current !== 1) {
+      this.arrowTime.emit(current-1)
+    }
+    else if (direction === "left" && current === 1) {
+      this.arrowTime.emit(this.pageInfo["total_pages"])
+    }
 
   }
 }
